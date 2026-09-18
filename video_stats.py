d4efccf74bc3d5b778d809dcddf2a212d92c58d6
benjaminbes1702 <bestbreurtjebenjamin@outlook.com>
2026-09-18 15:51:32 +0200

    Add Youtube uploads playlist and paginated video ID retrieval

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path=".env")
 
 api_key = os.getenv("API_KEY")
 channel_handle = "MrBeast"
+maxResults = 50
 
 def get_playlist_id(channel_handle, api_key):
     try:
@@ -23,5 +24,37 @@ def get_playlist_id(channel_handle, api_key):
         return None
     return channel_playlist_id
 
+def get_video_ids(playlistId, api_key):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={api_key}"
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            video_ids.extend([item["contentDetails"]["videoId"] for item in data["items"]])
+
+            pageToken = data.get("nextPageToken")
+            if not pageToken:
+                break
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while making the request: {e}")
+        return None
+
+    return video_ids
+
 if __name__ == "__main__":
-    get_playlist_id(channel_handle, api_key)
+    playlistId = get_playlist_id(channel_handle, api_key)
+    get_video_ids(playlistId, api_key)
+    
