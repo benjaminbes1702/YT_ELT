f0284b550cae3bd664b9839ce144db35d1fc9b6d
benjaminbes1702 <bestbreurtjebenjamin@outlook.com>
2026-09-21 21:18:49 +0200

    Add batched video metadata and statistics extraction

--- video_stats.py
+++ video_stats.py
@@ -54,7 +54,52 @@ def get_video_ids(playlistId, api_key):
 
     return video_ids
 
+
+
+def extract_video_data(video_ids, api_key):
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id: video_id + batch_size]
+
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id={video_ids_str}&key={api_key}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "published_at": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "view_count": statistics.get("viewCount", None),
+                    "like_count": statistics.get("likeCount", None),
+                    "comment_count": statistics.get("commentCount", None)
+                }
+
+                extracted_data.append(video_data)
+
+
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while making the request: {e}")
+        return None
+    
+    return extracted_data
+
 if __name__ == "__main__":
     playlistId = get_playlist_id(channel_handle, api_key)
-    get_video_ids(playlistId, api_key)
-    
+    video_ids = get_video_ids(playlistId, api_key)
+    extract_video_data(video_ids, api_key)
